47461afde2d965daa8740e6acc5be25263301631
rhrjsgh000 <rhrjsgh000@naver.com>
2025-05-14 20:32:17 +0900

    JVM 업데이트

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,12 +32,12 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
+//    kotlinOptions {
+//        jvmTarget = "21"
+//    }
 
     buildFeatures {
         viewBinding = true
